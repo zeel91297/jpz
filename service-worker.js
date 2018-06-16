d2eac701f6b3fb5fb7106909d8d99d43e647436c
zeel91297 <zeel91297@gmail.com>
2018-06-16 13:11:54 +0530

    initial

--- service-worker.js
+++ service-worker.js
@@ -1,73 +1,91 @@
-// Cache names
+// // Cache names
 
-var dataCacheName = 'JPZ'
+// var dataCacheName = 'JPZ'
 
-var cacheName = 'JPZPWA'
+// var cacheName = 'JPZPWA'
 
-// Application shell files to be cached
+// // Application shell files to be cached
 
-var filesToCache = [
-    '/'
-]
+// var filesToCache = [
+//     '/'
+// ]
 
-self.addEventListener('install', function (e) {
+// self.addEventListener('install', function (e) {
 
-    console.log('[ServiceWorker] Install')
+//     console.log('[ServiceWorker] Install')
 
-    e.waitUntil(
+//     e.waitUntil(
 
-        caches.open(cacheName).then(function (cache) {
+//         caches.open(cacheName).then(function (cache) {
 
-            console.log('[ServiceWorker] Caching app shell')
+//             console.log('[ServiceWorker] Caching app shell')
 
-            return cache.addAll(filesToCache)
+//             return cache.addAll(filesToCache)
 
+//         })
+
+//     )
+
+// })
+
+// self.addEventListener('activate', function (e) {
+
+//     console.log('[ServiceWorker] Activate')
+
+//     e.waitUntil(
+
+//         caches.keys().then(function (keyList) {
+
+//             return Promise.all(keyList.map(function (key) {
+
+//                 if (key !== cacheName && key !== dataCacheName) {
+
+//                     console.log('[ServiceWorker] Removing old cache', key)
+
+//                     return caches.delete(key)
+
+//                 }
+
+//             }))
+
+//         })
+
+//     )
+
+//     return self.clients.claim()
+
+// })
+
+// self.addEventListener('fetch', function (e) {
+
+//     console.log('[ServiceWorker] Fetch', e.request.url)
+
+//     e.respondWith(
+
+//         caches.match(e.request).then(function (response) {
+
+//             return response || fetch(e.request)
+
+//         })
+
+//     )
+
+// })
+
+var CACHE_NAME = 'JPZ';
+var urlsToCache = [
+    '/',
+    '/styles/main.css',
+    '/script/main.js'
+];
+
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function (cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
         })
-
-    )
-
-})
-
-self.addEventListener('activate', function (e) {
-
-    console.log('[ServiceWorker] Activate')
-
-    e.waitUntil(
-
-        caches.keys().then(function (keyList) {
-
-            return Promise.all(keyList.map(function (key) {
-
-                if (key !== cacheName && key !== dataCacheName) {
-
-                    console.log('[ServiceWorker] Removing old cache', key)
-
-                    return caches.delete(key)
-
-                }
-
-            }))
-
-        })
-
-    )
-
-    return self.clients.claim()
-
-})
-
-self.addEventListener('fetch', function (e) {
-
-    console.log('[ServiceWorker] Fetch', e.request.url)
-
-    e.respondWith(
-
-        caches.match(e.request).then(function (response) {
-
-            return response || fetch(e.request)
-
-        })
-
-    )
-
-})
+    );
+});
