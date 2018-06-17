33bcde153430ddf5b8ed34520ecc5f8e98c7376e
zeel91297 <zeel91297@gmail.com>
2018-06-17 16:24:39 +0530

    initial

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'JPZ';
+var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
     '/'
 ];
@@ -8,8 +8,24 @@ self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
         .then(function (cache) {
-            console.log('Opened cache');
             return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    console.log('Finally active. Ready to start serving content!');
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function (response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
         })
     );
 });
